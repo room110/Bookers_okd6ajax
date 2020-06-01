7243e931abd2f55b066c94d590fce198cd4bce04
yukiito <ito.room.room.110@gmail.com>
2020-06-01 19:25:00 +0900

    [Add]1

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'homes#top'
 
   resources :books
 
-  #post 'books' => 'books#create'
-
-  #get 'books' => 'books#index'
-
-  #get 'books/:id/edit' => 'books#edit', as:'edit_book'
-
-  #get 'books/:id' => 'books#show', as:'book'
-
-  #patch 'books/:id' => 'books#update'
-
-  #delete 'books/:id' => 'books#destroy'
-
-  devise_for :users
-
-  resources :users, only: [:show]
+resources :users, only: [:show, :edit, :update]
   
 end
